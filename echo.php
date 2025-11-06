a6d35f2f13711f1e265a889f969287d1919de8b6
Logesh <logu6165351@gmail.com>
2025-11-06 11:22:20 +0530

    Initial commit

---
+++ echo.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello hii World!<br>";
+echo "You're using PHP<br>";
+echo "You're in developer mode<br>";
+echo "PHP is core of Laravel<br>";
+echo "This is echo statement<br>";
+echo "You're seeing the basics<br>";
+echo "Using the echo<br>";
+echo "Being the developer<br>";
+echo "Something different<br>";
+?>
